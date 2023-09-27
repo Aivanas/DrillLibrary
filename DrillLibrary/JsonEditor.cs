36de14533a53be5c35994a38b93de3574705a868
Aivanas <vk53418006@gmail.com>
2023-09-27 12:05:33 +0300

    Добавьте файлы проекта.

---
+++ DrillLibrary/JsonEditor.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+
+namespace DrillLibrary
+{
+    public class JsonEditor
+    {
+        public static void Serialize<T>(T data, string path)
+        {
+            try
+            {
+                File.WriteAllText(path, JsonConvert.SerializeObject(data));
+            }
+            catch (Exception exception)
+            {
+                throw new Exception("Андрюха, у нас \n" + exception + "\n возможно криминал, по коням");
+               
+            }
+        }
+
+        public static T Deserialize<T>(string path)
+        {
+
+
+            try
+            {
+                T data = JsonConvert.DeserializeObject<T>(File.ReadAllText(path));
+                return data;
+            }
+            catch (Exception exception)
+            {
+                throw new Exception("Андрюха, у нас \n" + exception + "\n возможно криминал, по коням");
+
+            }
+            
+        }
+
+    }
+}
